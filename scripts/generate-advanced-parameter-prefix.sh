e68f49f4ed2c75f870d5f863b339474488b84ec2
keyanmaskoot <keyanmaskoot@gmail.com>
2026-06-28 13:17:59 +0200

    feat: example prefix generator script
    Unreviewed AI code.

--- scripts/generate-advanced-parameter-prefix.sh
+++ scripts/generate-advanced-parameter-prefix.sh
@@ -3,34 +3,53 @@ set -euo pipefail
 
 AIKEN="${AIKEN:-aiken}"
 BLUEPRINT="${BLUEPRINT:-plutus.json}"
-OUT="${OUT:-build/advanced-parameter-applied.json}"
+OUT_DIR="${OUT_DIR:-build}"
 ENV_FILE="${ENV_FILE:-env/default.ak}"
-PARAMETER="${PARAMETER:-5820000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f}"
-TITLE='"title":"examples/parameter_validation/advanced.parameterized_spend.spend"'
+MODULE="examples/parameter_validation/advanced"
+BYTES_PARAMETER="${BYTES_PARAMETER:-5820000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f}"
+INT_PARAMETER="${INT_PARAMETER:-1a000f4240}"
+LIST_PARAMETER="${LIST_PARAMETER:-83182a1903e81a000f4240}"
+CUSTOM_PARAMETER="${CUSTOM_PARAMETER:-d8798245aabbccddee1a000f4240}"
 
 "$AIKEN" build
-"$AIKEN" blueprint apply "$PARAMETER" \
-  -m examples/parameter_validation/advanced \
-  -v parameterized_spend \
-  -i "$BLUEPRINT" \
-  -o "$OUT"
 
-json="$(<"$OUT")"
-json="${json//[[:space:]]/}"
-validator_json="${json#*$TITLE}"
-compiled="${validator_json#*\"compiledCode\":\"}"
-compiled="${compiled%%\"*}"
-before_parameter="${compiled%%$PARAMETER*}"
+: >"$ENV_FILE"
 
-case "${compiled:0:2}" in
-  58) outer_header_hex_chars=4 ;;
-  59) outer_header_hex_chars=6 ;;
-  5a) outer_header_hex_chars=10 ;;
-  5b) outer_header_hex_chars=18 ;;
-  *) outer_header_hex_chars=2 ;;
-esac
+write_prefix() {
+  name="$1"
+  validator="$2"
+  parameter="$3"
+  out="$OUT_DIR/advanced-parameter-$validator.json"
+  title="\"title\":\"$MODULE.$validator.spend\""
 
-flat_prefix="${before_parameter:$outer_header_hex_chars:${#before_parameter} - outer_header_hex_chars - 2}"
+  "$AIKEN" blueprint apply "$parameter" \
+    -m "$MODULE" \
+    -v "$validator" \
+    -i "$BLUEPRINT" \
+    -o "$out"
 
-printf 'pub const flat_prefix_without_parameter_header = #"%s"\n' "$flat_prefix" >"$ENV_FILE"
+  json="$(<"$out")"
+  json="${json//[[:space:]]/}"
+  validator_json="${json#*$title}"
+  compiled="${validator_json#*\"compiledCode\":\"}"
+  compiled="${compiled%%\"*}"
+  before_parameter="${compiled%%$parameter*}"
+
+  case "${compiled:0:2}" in
+    58) outer_header_hex_chars=4 ;;
+    59) outer_header_hex_chars=6 ;;
+    5a) outer_header_hex_chars=10 ;;
+    5b) outer_header_hex_chars=18 ;;
+    *) outer_header_hex_chars=2 ;;
+  esac
+
+  flat_prefix="${before_parameter:$outer_header_hex_chars:${#before_parameter} - outer_header_hex_chars - 2}"
+
+  printf 'pub const %s = #"%s"\n\n' "$name" "$flat_prefix" >>"$ENV_FILE"
+}
+
+write_prefix flat_prefix_without_parameter_header parameterized_spend "$BYTES_PARAMETER"
+write_prefix int_flat_prefix_without_parameter_header parameterized_spend_int "$INT_PARAMETER"
+write_prefix list_flat_prefix_without_parameter_header parameterized_spend_list "$LIST_PARAMETER"
+write_prefix custom_flat_prefix_without_parameter_header parameterized_spend_custom "$CUSTOM_PARAMETER"
 "$AIKEN" fmt
